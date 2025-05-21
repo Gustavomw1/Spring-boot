62d064370f977f876e6dd1bd67c4b2b5123ff5cb
Gustavomw1 <166347161+Gustavomw1@users.noreply.github.com>
2025-05-21 20:43:03 -0300

    projeto finalizado

--- src/main/java/com/example/My_First_Spring/controllers/authController.java
+++ src/main/java/com/example/My_First_Spring/controllers/authController.java
@@ -1,6 +1,8 @@
 package com.example.My_First_Spring.controllers;
 
+import java.lang.classfile.ClassFile.Option;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.My_First_Spring.model.usuario;
+import com.example.My_First_Spring.security.jwtUtil;
 import com.example.My_First_Spring.service.usuarioService;
 
 @RestController
@@ -27,4 +30,13 @@ public class authController {
         return ResponseEntity.ok(usuario);
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> request) {
+        Optional<usuario> usuario = usuarioService.buscarPorUsername(request.get("username"));
+        if (usuario.isPresent() && usuario.get().getPassword().equals(request.get("password"))) {
+            String token = jwtUtil.generateToken(usuario.get().getUsername());
+            return ResponseEntity.ok(Map.of("token", token));
+        }
+        return ResponseEntity.status(401).body("Credenciais invalida");
+    }
 }
